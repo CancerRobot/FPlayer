630cc90eff55ce2f6555010c0aa1159d0a7d71a1
CancerRobot <345618908@qq.com>
2017-05-22 15:16:10 +0800

    很丑很丑的UI

--- app/src/main/java/com/wozipa/android/study/ui/HomeAwardActivity.java
+++ app/src/main/java/com/wozipa/android/study/ui/HomeAwardActivity.java
@@ -15,6 +15,21 @@ public class HomeAwardActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_award);
 
+        ImageView imageView1= (ImageView) findViewById(R.id.award_head_clock);
+        imageView1.setImageResource(R.drawable.clock);
+
+        ImageView imageView2= (ImageView) findViewById(R.id.award_head_gift);
+        imageView2.setImageResource(R.drawable.gift);
+
+        ImageView imageView3= (ImageView) findViewById(R.id.award_head_notify);
+        imageView3.setImageResource(R.drawable.notify1);
+
+        ImageView imageView4= (ImageView) findViewById(R.id.award_exchange_img);
+        imageView4.setImageResource(R.drawable.flag );
+
+        ImageView imageView5= (ImageView) findViewById(R.id.award_delete_img);
+        imageView5.setImageResource(R.drawable.remove);
+
         Button button1=(Button)findViewById(R.id.home_award_2_act);
         button1.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
